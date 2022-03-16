2d49d20c2a5c64e6f946ce81a1208a988608ef62
iamalanxue <alanxue43@gmail.com>
2022-03-15 21:54:32 -0500

    finished front end

--- src/containers/Upload.tsx
+++ src/containers/Upload.tsx
@@ -5,7 +5,7 @@ import { useState } from "react"
 
 const Upload = () => {
   const [file, setFile] = useState<File | undefined>(undefined);
-
+  const [result, setResult] = useState<String>(""); 
   const chooseFile = (event: BaseSyntheticEvent) => {
     // console.log(event.target.files); 
     // console.log(event)
@@ -24,11 +24,36 @@ const Upload = () => {
         body: data
       }
       fetch('http://127.0.0.1:5000/upload', options)
-        .then(response => {
-          console.log(response)
-          console.log(response.body)
-          console.log(response.body?.getReader())
-          return response.body
+        .then(response => response.body)
+        .then(body => {
+          const reader = body?.getReader()
+          console.log(reader)
+          reader?.read().then(function processText({done, value}) {
+            let temp = ""
+            let data = value ? value.length : 10
+            for(var i = 0; i < data; i++) {
+              let c = value ? value[i] : 80
+              var char = String.fromCharCode(c); 
+              if (char.match(/[^\r\n]+/g) !== null) {
+                temp += char
+              } else{
+                temp += char
+              }
+            }
+            setResult(temp);
+            console.log(temp)
+            let csvData = new Blob([temp], { type: 'text/csv' });  
+            let csvUrl = URL.createObjectURL(csvData);
+
+            let hiddenElement = document.createElement('a');
+            hiddenElement.href = csvUrl;
+            hiddenElement.target = '_blank';
+            hiddenElement.download = 'out' + '.csv';
+            hiddenElement.click();
+          })
+        })
+        .catch(err => {
+          console.log(err)
         })
     }
   }
